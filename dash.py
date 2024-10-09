5f646ae48767d36b0fdafc3f3040a25ed5d1d814
DMLen <dmlennox03@gmail.com>
2024-10-10 06:01:51 +1100

    90% of the way there i think

--- dash.py
+++ dash.py
@@ -47,6 +47,10 @@ def dash(graph, deletednode, deletednodeneighbours):
     #print(f"list of neighbors of node 1: {get_neighbors(graph, 1)}") #debug
     print(f"Most recently deleted node: {deletednode}, Neighbours of deleted node: {deletednodeneighbours}") #debug
 
+    if len(deletednodeneighbours) == 1:
+        print("Only one neighbour, no need to heal.")
+        return graph, []
+
 
     binarytree = btree()
 
@@ -54,16 +58,26 @@ def dash(graph, deletednode, deletednodeneighbours):
     sorted_deletednodeneighbours = sorted(deletednodeneighbours, key=lambda id: graph.nodes[id]['delta'])
     print(f"Sorted neighbours by delta values: {sorted_deletednodeneighbours}")
 
+    for node_id in sorted_deletednodeneighbours:
+        delta_value = graph.nodes[node_id]['delta']
+        print(f"Node ID: {node_id}, Delta Value: {delta_value}")
+
+
     for id in sorted_deletednodeneighbours:
         binarytree.insert(id)
         print(f"Inserted node {id} into binary tree.")
 
     #2. add edges between nodes of graph based on how they are connected in binary tree
     traversed = binarytree.inorder_traversal()
+    new_edges = [] #list of new edges created via healing
 
     for i in range(len(traversed) - 1):
-        graph.add_edge(traversed[i], traversed[i+1])
-        print(f"Added edge between {traversed[i]} and {traversed[i+1]}")
+        if not graph.has_edge(traversed[i], traversed[i+1]):
+            graph.add_edge(traversed[i], traversed[i+1])
+            new_edges.append((traversed[i], traversed[i+1])) #add new edge to list
+            print(f"Added edge between {traversed[i]} and {traversed[i+1]}")
+        else:
+            print(f"Edge between {traversed[i]} and {traversed[i+1]} already exists.")
 
     #3. propagate minimum DashID of neighbour nodes to all nodes processed in this operation
     #find minimum DashID of deletednodeneighbours
@@ -73,8 +87,8 @@ def dash(graph, deletednode, deletednodeneighbours):
     #propagate minimum DashID to all nodes processed in this operation
     for id in traversed:
         graph.nodes[id]['dashID'] = min_dashID
+        graph.nodes[id]['delta'] += 1 #
 
         print(f"Propagated DashID {min_dashID} to node {id}")   
-    
-    print(f"=== DASH operation complete ===")
-    return graph
+
+    return graph, new_edges
--- netgraph.py
+++ netgraph.py
@@ -6,25 +6,53 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import random
 from matplotlib.colors import Normalize
 from matplotlib.cm import ScalarMappable
+from collections import defaultdict
 
 import dash
 
-def get_unique_neighbors(graph, node):
-        #get unique neighbours of a node
-        neighbors = list(graph.neighbors(node))
+##TODO GET NON-UNIQUE NEIGHBOURS OF A NODE THAT WERE INTRODUCED VIA HEALING EDGES
 
-        #remove neighbours that share the same dashID
-        target_dashID = graph.nodes[node].get('dashID')
+def partition(graph, node):
+    neighbors = list(graph.neighbors(node))
+    print(f"Neighbors of deleted node {node}: {neighbors}")
 
-        unique_neighbors = []
-        for neighbor in neighbors:
-            if graph.nodes[neighbor].get('dashID') != target_dashID:
-                unique_neighbors.append(neighbor)
-                print(f"Node {neighbor} has a different dashID than {node} ({target_dashID})")
-            else:
-                print(f"Node {neighbor} has the same dashID as {node} ({target_dashID}). Excluding from unique neighbours.")
+    # Partitioning the neighbors based on their IDs
+    partitions = {}
+    for neighbor in neighbors:
+        # Assuming each node has a randomly assigned ID
+        node_id = graph.nodes[neighbor]['dashID']
+        if node_id not in partitions:
+            partitions[node_id] = []
+        partitions[node_id].append(neighbor)
+        print(f"Neighbor {neighbor} with dashID {node_id} added to partition")
+
+    # Select unique representatives from each partition
+    unique_neighbors = []
+    for partition_id, partition in partitions.items():
+        # Choose the neighbor with the lowest initial ID
+        representative = min(partition, key=lambda x: graph.nodes[x]['initial_dashID'])
+        unique_neighbors.append(representative)
+        print(f"Partition {partition_id} representatives: {partition}, selected: {representative} (via initial ID {graph.nodes[representative]['initial_dashID']})")
+
+    print(f"Unique neighbors for deleted node {node}: {unique_neighbors}")
+    return unique_neighbors
+
+def connect_graph(graph):
+    if nx.is_connected(graph):
+        return graph
+    
+    components = list(nx.connected_components(graph))
+    largest_component = components[0]
+
+    for component in components[1:]:
+        node_in_largest = next(iter(largest_component))
+        node_in_component = next(iter(component))
         
-        return unique_neighbors
+        graph.add_edge(node_in_largest, node_in_component)
+        
+        largest_component.update(component)
+    
+    return graph
 
 class NetGraph:
     def __init__(self, root):
@@ -63,11 +91,15 @@ class NetGraph:
         self.dash_button = tk.Button(self.frame, text="DASH Healing Step", command=self.DASH_healingstep, bg='green')
         self.dash_button.pack()
 
+        self.debug_button = tk.Button(self.frame, text="DEBUG", command=self.debug, bg='blue')
+        self.debug_button.pack()
+
         self.canvas = None
         self.G = None #g = graph
-        self.pos = {} #dictionary to store node positions
-        self.last_deleted_node = None
-        self.last_deleted_node_neighbours_list = None #contains unique neighbours (not sharing the same dashID) of the last deleted node
+        self.healcount = 1
+
+    def debug(self):
+        print(partition(self.G, 1))
 
     def generate_graph(self): #for initial generation
         try:
@@ -75,20 +107,27 @@ class NetGraph:
             if nodenumber <= 0:
                 raise ValueError("Number of nodes must be positive.")
             
+            self.pos = {} #dictionary to store node positions
+            self.last_deleted_node = None
+            self.last_deleted_node_neighbours_list = None #contains unique neighbours (not sharing the same dashID) of the last deleted node
+            self.new_edges = [] #list of new edges created via healing
+            
             #higher power = smaller radius
             power = 0.5
             radius = 1.0 / (nodenumber ** power) #make radius smaller if more nodes
 
             #use networkx to generate a random graph.
-            while True:
-                self.G = nx.random_geometric_graph(n=nodenumber, radius=radius)
-                if nx.is_connected(self.G):
-                    break
+            self.G = nx.random_geometric_graph(n=nodenumber, radius=radius)
+            self.G = connect_graph(self.G)
+            
 
             #initialize node deltas (degrees changed) and DASH ID
             for node in self.G.nodes():
+                id = random.random() #random float between 0 and 1
+                
                 self.G.nodes[node]['delta'] = 0
-                self.G.nodes[node]['dashID'] = random.random() #random float between 0 and 1
+                self.G.nodes[node]['dashID'] = id
+                self.G.nodes[node]['initial_dashID'] = id
 
             #if previous graph exists, clear it
             if self.canvas:
@@ -119,10 +158,12 @@ class NetGraph:
             for node in self.G.nodes()
         ] #if node has connections, colour is determined by normalised colormap. lower bound is 1, upper bound is current maximum degree within the network (within the current iteration)
 
-        labels = {node: f"ID: {node}" for node in self.G.nodes()}
+        labels = {node: f"ID: {node}\nDashID: {self.G.nodes[node].get('dashID')}\nDelta: {self.G.nodes[node].get('delta')}" for node in self.G.nodes()}
         
         #draw network
         nx.draw(self.G, self.pos, ax=ax, with_labels=True, labels=labels, node_color=node_colors, node_size=500, font_size=8, edge_color='grey')
+        self.valid_new_edges = [(u, v) for u, v in self.new_edges if u in self.G.nodes() and v in self.G.nodes()]
+        nx.draw_networkx_edges(self.G, self.pos, edgelist=self.valid_new_edges, edge_color='red', ax=ax)
 
         #add colorbar
         scalarmap = plt.cm.ScalarMappable(cmap=colormap, norm=normalized)
@@ -141,7 +182,7 @@ class NetGraph:
 
         node_deletion_target = random.choice(list(self.G.nodes()))
         self.last_deleted_node = node_deletion_target
-        self.last_deleted_node_neighbours_list = get_unique_neighbors(self.G, node_deletion_target)
+        self.last_deleted_node_neighbours_list = partition(self.G, node_deletion_target)
         self.G.remove_node(node_deletion_target)
 
         if node_deletion_target in self.pos:
@@ -158,7 +199,7 @@ class NetGraph:
         
         max_node = max(self.G.degree, key=lambda x: x[1])[0] #get node with max edge count
         self.last_deleted_node = max_node
-        self.last_deleted_node_neighbours_list = get_unique_neighbors(self.G, max_node)
+        self.last_deleted_node_neighbours_list = partition(self.G, max_node)
         self.G.remove_node(max_node)
 
         if max_node in self.pos:
@@ -175,11 +216,21 @@ class NetGraph:
     #network healing (singular step) per Degree Assisted Self-Healing algorithm described by Dr Amitabh Trehan
     #currently unimplemented
     def DASH_healingstep(self):
-        self.G = dash.dash(self.G, self.last_deleted_node, self.last_deleted_node_neighbours_list)
+
+        print(f"=== DASH operation {self.healcount} initiated ===")
+        returned_new_edges = []
+        self.G, returned_new_edges = dash.dash(self.G, self.last_deleted_node, self.last_deleted_node_neighbours_list)
+
+        for i in returned_new_edges:
+            self.new_edges.append(i)
+
+        print(f"New edges created: {self.new_edges}")
 
         plt.close('all') #clear prev figure
         self.canvas.get_tk_widget().destroy() #clear prev canvas
         self.draw_graph()
+        self.healcount += 1
+        print(f"=== DASH operation complete ===")
 
 #main control loop
 if __name__ == "__main__":
