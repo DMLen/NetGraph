e00fe0523148c1928af59e96fa86f2928778547d
DMLen <dmlennox03@gmail.com>
2024-10-10 02:54:06 +1100

    groundwork for dash implementation

--- netgraph.py
+++ netgraph.py
@@ -7,6 +7,25 @@ import random
 from matplotlib.colors import Normalize
 from matplotlib.cm import ScalarMappable
 
+import dash
+
+def get_unique_neighbors(graph, node):
+        #get unique neighbours of a node
+        neighbors = list(graph.neighbors(node))
+
+        #remove neighbours that share the same dashID
+        target_dashID = graph.nodes[node].get('dashID')
+
+        unique_neighbors = []
+        for neighbor in neighbors:
+            if graph.nodes[neighbor].get('dashID') != target_dashID:
+                unique_neighbors.append(neighbor)
+                print(f"Node {neighbor} has a different dashID than {node} ({target_dashID})")
+            else:
+                print(f"Node {neighbor} has the same dashID as {node} ({target_dashID}). Excluding from unique neighbours.")
+        
+        return unique_neighbors
+
 class NetGraph:
     def __init__(self, root):
         self.root = root
@@ -32,15 +51,23 @@ class NetGraph:
         self.delete_button = tk.Button(self.frame, text="Delete [Random]", command=self.delete_random_node, bg='orangered')
         self.delete_button.pack()
 
-        self.delete_max_button = tk.Button(self.frame, text="Delete [Most Connections] [UNIMPLEMENTED]", command=self.delete_max_node, bg='orangered')
+        self.delete_max_button = tk.Button(self.frame, text="Delete [Most Connections]", command=self.delete_max_node, bg='orangered')
         self.delete_max_button.pack()
 
         self.delete_maxneighbour_button = tk.Button(self.frame, text="Delete [Neighbour of Most Connections] [UNIMPLEMENTED]", command=self.delete_maxneighbour_node, bg='orangered')
         self.delete_maxneighbour_button.pack()
 
+        self.healing_label = tk.Label(self.frame, text="=== Healing Strategies ===")
+        self.healing_label.pack()
+
+        self.dash_button = tk.Button(self.frame, text="DASH Healing Step", command=self.DASH_healingstep, bg='green')
+        self.dash_button.pack()
+
         self.canvas = None
         self.G = None #g = graph
         self.pos = {} #dictionary to store node positions
+        self.last_deleted_node = None
+        self.last_deleted_node_neighbours_list = None #contains unique neighbours (not sharing the same dashID) of the last deleted node
 
     def generate_graph(self):
         try:
@@ -54,6 +81,11 @@ class NetGraph:
 
             #use networkx to generate a random graph.
             self.G = nx.random_geometric_graph(n=nodenumber, radius=radius)
+
+            #initialize node deltas (degrees changed) and DASH ID
+            for node in self.G.nodes():
+                self.G.nodes[node]['delta'] = 0
+                self.G.nodes[node]['dashID'] = random.random() #random float between 0 and 1
 
             #if previous graph exists, clear it
             if self.canvas:
@@ -82,7 +114,7 @@ class NetGraph:
         node_colors = [
             '#515151' if degrees[node] == 0 else colormap(normalized(degrees[node]))
             for node in self.G.nodes()
-        ]
+        ] #if node has connections, colour is determined by normalised colormap. lower bound is 1, upper bound is current maximum degree within the network (within the current iteration)
 
         labels = {node: f"ID: {node}" for node in self.G.nodes()}
         
@@ -105,6 +137,8 @@ class NetGraph:
             return
 
         node_deletion_target = random.choice(list(self.G.nodes()))
+        self.last_deleted_node = node_deletion_target
+        self.last_deleted_node_neighbours_list = get_unique_neighbors(self.G, node_deletion_target)
         self.G.remove_node(node_deletion_target)
 
         if node_deletion_target in self.pos:
@@ -115,7 +149,22 @@ class NetGraph:
         self.draw_graph()
 
     def delete_max_node(self):
-        print("dummy function!")
+        if self.G is None or len(self.G) == 0:
+            messagebox.showwarning("Warning", "No graph exists!")
+            return
+        
+        max_node = max(self.G.degree, key=lambda x: x[1])[0] #get node with max edge count
+        self.last_deleted_node = max_node
+        self.last_deleted_node_neighbours_list = get_unique_neighbors(self.G, max_node)
+        self.G.remove_node(max_node)
+
+        if max_node in self.pos:
+            del self.pos[max_node] #remove node's position from dictionary
+
+        plt.close('all') #clear prev figure
+        self.canvas.get_tk_widget().destroy() #clear prev canvas
+        self.draw_graph()
+
 
     def delete_maxneighbour_node(self):
         print("dummy function!")
@@ -123,7 +172,11 @@ class NetGraph:
     #network healing (singular step) per Degree Assisted Self-Healing algorithm described by Dr Amitabh Trehan
     #currently unimplemented
     def DASH_healingstep(self):
-        print("dummy function!")
+        self.G = dash.dash(self.G, self.last_deleted_node, self.last_deleted_node_neighbours_list)
+
+        plt.close('all') #clear prev figure
+        self.canvas.get_tk_widget().destroy() #clear prev canvas
+        self.draw_graph()
 
 #main control loop
 if __name__ == "__main__":
